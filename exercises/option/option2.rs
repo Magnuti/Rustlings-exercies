1a9c518311522de25be2817c1535d530c8a93b34
Magnus <magnustidemann@gmail.com>
2020-06-07 15:24:10 +0200

    Clippy, option and error done

--- exercises/option/option2.rs
+++ exercises/option/option2.rs
@@ -1,8 +1,6 @@
 // option2.rs
 // Make me compile! Execute `rustlings hint option2` for hints
 
-// I AM NOT DONE
-
 fn main() {
     let optional_value = Some(String::from("rustlings"));
     if let value = optional_value {
@@ -16,13 +14,8 @@ fn main() {
         optional_values_vec.push(Some(x));
     }
 
-    // TODO: make this a while let statement - remember that vector.pop also adds another layer of Option<T>
-    // You can stack `Option<T>`'s into while let and if let
-    while let value = optional_values_vec.pop() {
-        // if value == None {
-            // println!("Done");
-        // } else {
-            println!("current value: {}", value.unwrap().unwrap());
-        // }
+    while let Some(Some(value)) = optional_values_vec.pop() {
+        // remember that vector.pop also adds another layer of Option<T>
+        println!("current value: {}", value);
     }
 }
